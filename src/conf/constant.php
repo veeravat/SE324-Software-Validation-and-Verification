54e7dad87a46b02df8c79ff35f7f598968926a57
TuyMove JVT <TuyMove JVT>
2017-02-01 02:59:35 +0700

    azure complatible

--- src/conf/constant.php
+++ src/conf/constant.php
@@ -1,4 +1,32 @@
 <?php
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+        continue;
+    }
+
+    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+if($servername){
+return $clasa = json_decode(json_encode([
+    'setting' => [
+        'db' => [
+            'driver'    => 'mysql',
+            'host'      => $servername,
+            'database'  => $dbname,
+            'username'  => $username,
+            'password'  => $password,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix'    => ''
+        ]
+    ]
+]));
+}
+else{
 return $clasa = json_decode(json_encode([
     'setting' => [
         'db' => [
@@ -13,3 +41,4 @@ return $clasa = json_decode(json_encode([
         ]
     ]
 ]));
+}
